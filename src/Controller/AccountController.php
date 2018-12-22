9328bb2fa8c5f9d82b5da366818b04a552166cc3
LeCuistotduWeb <gaetan.boyron@gmail.com>
2018-12-22 16:35:46 +0100

    feat: allow to change a userPassword

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,11 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\PasswordUpdate;
 use App\Entity\User;
 use App\Form\AccountType;
+use App\Form\PasswordUpdateType;
 use App\Form\RegistrationType;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FormError;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -92,31 +95,49 @@ class AccountController extends AbstractController
     }
 
     /**
+     * Permet de modifier le mot de passe
+     * @Route("/account/password-update", name="account_password")
+     * @return Response
+     */
+    public function updatePassword(Request $request, UserPasswordEncoderInterface $encoder, ObjectManager $manager){
+        $passwordUpdate = New PasswordUpdate();
+        $user = $this->getUser();
+
+        $form = $this->createForm(PasswordUpdateType::class, $passwordUpdate);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+            if(!password_verify($passwordUpdate->getOldPassword(), $user->getPassword())){
+                //gerer l'erreur
+                $form->get('oldPassword')->addError(new FormError("Le mot de passe que vous avez entré n'est pas votre mot de passe actuel !"));
+            }else{
+                $newPassword = $passwordUpdate->getNewPassword();
+                $hash = $encoder->encodePassword($user, $newPassword);
+
+                $user->setPassword($hash);
+
+                $manager->persist($user);
+                $manager->flush();
+
+                $this->addFlash(
+                    'success',
+                    "Votre mot de passe a bien été modifié !"
+                );
+
+                return $this->redirectToRoute('home');
+            }
+        }
+        return $this->render('account/password.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
      * Permet de se déconnecter
      * @Route("/logout", name="account_logout")
      * @return void
      */
     public function logout() {}
-
-    /**
-     *
-     */
-    public function accountInfo()
-    {
-        // allow any authenticated user - we don't care if they just
-        // logged in, or are logged in via a remember me cookie
-        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_REMEMBERED');
-    }
-
-    /**
-     *
-     */
-    public function resetPassword()
-    {
-        // require the user to log in during *this* session
-        // if they were only logged in via a remember me cookie, they
-        // will be redirected to the login page
-        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
-        // ...
-    }
 }
+
