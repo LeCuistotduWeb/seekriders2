c82f151e973a34c2ae544e1f7901d665a9a7760b
LeCuistotduWeb <gaetan.boyron@gmail.com>
2019-02-03 16:43:39 +0100

    refactor migrations

--- src/Migrations/Version20190203154211.php
+++ src/Migrations/Version20190203154211.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190203152737 extends AbstractMigration
+final class Version20190203154211 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -25,14 +25,15 @@ final class Version20190203152737 extends AbstractMigration
         $this->addSql('CREATE TABLE location (id INT AUTO_INCREMENT NOT NULL, country VARCHAR(255) NOT NULL, region VARCHAR(255) NOT NULL, department VARCHAR(255) NOT NULL, city VARCHAR(255) NOT NULL, address VARCHAR(255) NOT NULL, post_code VARCHAR(255) NOT NULL, longitude DOUBLE PRECISION NOT NULL, latitude DOUBLE PRECISION NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('CREATE TABLE role (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('CREATE TABLE spot (id INT AUTO_INCREMENT NOT NULL, author_id INT NOT NULL, location_id INT NOT NULL, title VARCHAR(255) NOT NULL, description LONGTEXT NOT NULL, paying INT NOT NULL, type INT NOT NULL, created_at DATETIME NOT NULL, INDEX IDX_B9327A73F675F31B (author_id), UNIQUE INDEX UNIQ_B9327A7364D218E (location_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE spot_picture (id INT AUTO_INCREMENT NOT NULL, spot_pictures_id INT NOT NULL, filename VARCHAR(255) NOT NULL, INDEX IDX_5C317B21256499CB (spot_pictures_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, location_id INT DEFAULT NULL, firstname VARCHAR(255) DEFAULT NULL, surname VARCHAR(255) DEFAULT NULL, username VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, biography LONGTEXT DEFAULT NULL, created_at DATETIME NOT NULL, updated_at DATETIME DEFAULT NULL, birthday_at DATETIME DEFAULT NULL, reset_token VARCHAR(255) DEFAULT NULL, avatar VARCHAR(255) DEFAULT NULL, INDEX IDX_8D93D64964D218E (location_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('CREATE TABLE user_role (user_id INT NOT NULL, role_id INT NOT NULL, INDEX IDX_2DE8C6A3A76ED395 (user_id), INDEX IDX_2DE8C6A3D60322AC (role_id), PRIMARY KEY(user_id, role_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('ALTER TABLE spot ADD CONSTRAINT FK_B9327A73F675F31B FOREIGN KEY (author_id) REFERENCES user (id)');
         $this->addSql('ALTER TABLE spot ADD CONSTRAINT FK_B9327A7364D218E FOREIGN KEY (location_id) REFERENCES location (id)');
+        $this->addSql('ALTER TABLE spot_picture ADD CONSTRAINT FK_5C317B21256499CB FOREIGN KEY (spot_pictures_id) REFERENCES spot (id)');
         $this->addSql('ALTER TABLE user ADD CONSTRAINT FK_8D93D64964D218E FOREIGN KEY (location_id) REFERENCES location (id)');
         $this->addSql('ALTER TABLE user_role ADD CONSTRAINT FK_2DE8C6A3A76ED395 FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE user_role ADD CONSTRAINT FK_2DE8C6A3D60322AC FOREIGN KEY (role_id) REFERENCES role (id) ON DELETE CASCADE');
-        $this->addSql('ALTER TABLE spot_picture ADD CONSTRAINT FK_5C317B21256499CB FOREIGN KEY (spot_pictures_id) REFERENCES spot (id)');
     }
 
     public function down(Schema $schema) : void
@@ -49,6 +50,7 @@ final class Version20190203152737 extends AbstractMigration
         $this->addSql('DROP TABLE location');
         $this->addSql('DROP TABLE role');
         $this->addSql('DROP TABLE spot');
+        $this->addSql('DROP TABLE spot_picture');
         $this->addSql('DROP TABLE user');
         $this->addSql('DROP TABLE user_role');
     }
