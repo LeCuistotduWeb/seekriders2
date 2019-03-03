31cbf24a945169a7a4cd265e3e9e07b37b66dc31
LeCuistotduWeb <gaetan.boyron@gmail.com>
2019-03-03 14:51:21 +0100

    refactor: refactor data fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,6 +4,7 @@ namespace App\DataFixtures;
 
 use App\Entity\Location;
 use App\Entity\Spot;
+use App\Entity\SpotLike;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
@@ -33,37 +34,28 @@ class AppFixtures extends Fixture
         $manager->persist($adminUser);
 
         $users = [];
-        $locations = [];
         $spots = [];
 
-        /* creation des adresses*/
-        for ($i=0; $i < 100; $i++){
-            $location = new Location();
+        /* creation des utilisateurs*/
+        for ($i=0; $i < 10; $i++) {
 
-            $location->setDepartment($faker->departmentName)
+            $userLocation = new Location();
+            $userLocation->setDepartment($faker->departmentName)
                 ->setPostCode(str_replace(' ', '', $faker->postcode))
                 ->setRegion($faker->region)
                 ->setCountry($faker->country)
                 ->setCity($faker->city)
                 ->setAddress($faker->address)
                 ->setLatitude($faker->latitude)
-                ->setLongitude($faker->longitude)
-            ;
-            $manager->persist($location);
-            $locations[] = $location;
-        }
+                ->setLongitude($faker->longitude);
+            $manager->persist($userLocation);
 
-        $location = $locations[mt_rand(0, count($locations) - 1)];
-
-        /* creation des utilisateurs*/
-        for ($i=0; $i < 10; $i++) {
             $user = new User();
-
             $user->setSurname($faker->name)
                 ->setFirstname($faker->firstName)
                 ->setUsername($faker->userName)
                 ->setBirthdayAt($faker->dateTimeBetween($startDate = '-30 years', $endDate = '-3 years'))
-                ->setLocation($location)
+                ->setLocation($userLocation)
                 ->setEmail($faker->email)
                 ->setCreatedAt($faker->dateTime($max = 'now', $timezone = null))
                 ->setBiography($faker->text)
@@ -72,29 +64,38 @@ class AppFixtures extends Fixture
             $manager->persist($user);
             $users[] = $user;
 
-            $location = $locations[mt_rand(0, count($locations) - 1)];
             /* creation des spots*/
+            $spotLocation = new Location();
+            $spotLocation->setDepartment($faker->departmentName)
+                ->setPostCode(str_replace(' ', '', $faker->postcode))
+                ->setRegion($faker->region)
+                ->setCountry($faker->country)
+                ->setCity($faker->city)
+                ->setAddress($faker->address)
+                ->setLatitude($faker->latitude)
+                ->setLongitude($faker->longitude)
+            ;
+            $manager->persist($spotLocation);
 
             $spot = new Spot();
-
             $spot->setTitle($faker->sentence($nbWords = 6, $variableNbWords = true))
                 ->setDescription($faker->paragraph($nbSentences = 3, $variableNbSentences = true))
-                ->setLocation($location)
+                ->setLocation($spotLocation)
                 ->setType(mt_rand(0, count(Spot::SPOT_TYPE) -1))
                 ->setPaying(mt_rand(0, count(Spot::PRICE) -1))
                 ->setCreatedAt($faker->dateTimeBetween($startDate = '-1 years', $endDate = '-1 days'))
                 ->setAuthor($user);
             $manager->persist($spot);
             $spots[] = $spot;
-        }
 
-        // random spotLike
-//        for ($i = 0; $i < mt_rand(0,10); $i++){
-//            $like = new SpotLike();
-//            $like->setSpot($spot)
-//                ->setUser($faker->randomElement($users));
-//            $manager->persist($like);
-//        }
+            //random spotLike
+            for ($i = 0; $i < mt_rand(0,10); $i++){
+                $like = new SpotLike();
+                $like->setSpot($faker->randomElement($spots))
+                    ->setUser($faker->randomElement($users));
+                $manager->persist($like);
+            }
+        }
 
         $manager->flush();
     }
